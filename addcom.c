52b6bf76ba6837463088057efc951ddb34b078c6
HyperWin <92534013+HyperWinX@users.noreply.github.com>
2023-11-21 18:33:43 +0400

    Added installation

--- addcom.c
+++ addcom.c
@@ -2,10 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/sendfile.h>
+#include <sys/stat.h>
 
 int main(int argc, char* argv[]){
+    if (argc == 2 && strcmp(argv[1], "install") == 0){
+        char current_path[1024];
+        if (getcwd(current_path, sizeof(current_path)) == NULL){
+            fprintf(stderr, "Cannot get current directory, please install program yourself or run as root.\n");
+            return -1;
+        }
+        strcat(current_path, "/");
+        strcat(current_path, argv[0]);
+        char command[1536];
+        strcpy(command, "cp ");
+        strcat(command, current_path);
+        strcat(command, " /bin/addcom > /dev/null");
+        printf(command);
+        scanf(NULL);
+        int retcode = system(command);
+        switch (retcode){
+            case -1:
+                fprintf(stderr, "Installation failure!\n");
+                return -1;
+            case 0:
+                fprintf(stdout, "Installation successful!\n");
+                return 0;
+        }
+    }
     if (argc != 3){
-        printf("Incorrect usage!\nUsage:\taddcom <path_to_executable> <name_to_run>");
+        printf("Incorrect usage!\nUsage:\taddcom <path_to_executable> <name_to_run>\n");
         return -1;
     }
     if (!fopen(argv[1], "r")){
